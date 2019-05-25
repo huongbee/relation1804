3869bfe2750953d328a88506a30931774b237a7d
Huong <huongnguyenak96@gmail.com>
2019-05-25 20:49:59 +0700

    4.1

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 require('./lib/dbconnect');
+const { hash } = require('./lib/bcrypt');
 const { UserModel } = require('./models/User.model');
+//4.2
 
 //4.1
-UserModel.insertMany([
-    {
-        email: 'admin@gmail.com',
-        password: '123456' //
-    }
-])
+// hash('111111')
+// .then(hash=> UserModel.insertMany([
+//     {
+//         email: 'admin02@gmail.com',
+//         password: hash,
+//         name: 'Nguyen Admin 02'
+//     },
+//     {
+//         email: 'guest@gmail.com',
+//         password: hash,
+//         name: 'Nguyen guest'
+//     },
+//     {
+//         email: 'manager@gmail.com',
+//         password: hash,
+//         name: 'Manager'
+//     }
+// ]))
+// .then(users=>console.log(users))
+// .catch(err=>console.log(err.message))
+
